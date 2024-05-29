f7e6be26ac140b8d2fd10eb23a901a4af0fe05db
neufrankie <neu.vietanh272@gmail.com>
2024-05-30 06:09:16 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+internal class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Nhap vao nhiet do can chuyen doi (°C): ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        Console.WriteLine("Nhiet do da duoc chuyen doi la (°F): " + fahrenheit);
+    }
+}
